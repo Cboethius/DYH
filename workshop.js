fba273d9d3d1ba39685e2dd798258acf4770c946
Christian Boethius <christian@boethius.ch>
2021-12-10 06:20:19 +0100

    workshop2

--- workshop.js
+++ workshop.js
@@ -9,3 +9,39 @@ const btn2 = $('.buttonpic2')
 btn2.click( function() {
   $('.popup2').slideToggle(400)
 })
+
+const btn3 = $('.buttonpic3')
+
+btn3.click( function() {
+  $('.popup3').slideToggle(400)
+})
+
+const btn4 = $('.buttonpic4')
+
+btn4.click( function() {
+  $('.popup4').slideToggle(400)
+})
+
+const btn5 = $('.buttonpic5')
+
+btn5.click( function() {
+  $('.popup5').slideToggle(400)
+})
+
+const btn6 = $('.buttonpic6')
+
+btn6.click( function() {
+  $('.popup6').slideToggle(400)
+})
+
+const btn7 = $('.buttonpic7')
+
+btn7.click( function() {
+  $('.popup7').slideToggle(400)
+})
+
+const btn8 = $('.buttonpic8')
+
+btn8.click( function() {
+  $('.popup8').slideToggle(400)
+})
